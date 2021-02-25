542a91c4664da071a556d2a3fc4f36df8d0d0d00
Jaroslav Khorishchenko <websnipter@gmail.com>
2021-02-25 13:35:41 +0200

    DO-36: Recovery screen email filed verification fixes

--- src/screens/Auth/RecoverPass/index.tsx
+++ src/screens/Auth/RecoverPass/index.tsx
@@ -1,16 +1,14 @@
 import { Grid } from '@material-ui/core';
 import { AuthFormContainer, AuthScreenBackground } from 'components/Auth';
-import { Logo, ScreenTitle, Title, Text, View, TextLink, useSnackbar } from 'components/Common';
-import { TextInput } from 'components/Forms';
-import React, { ChangeEvent, FC, useState } from 'react';
-import { isCognitoErrResponse, useAuth } from 'core/api';
-import { globalStyles, m, StyleProps } from 'styles';
-import { errToStr, Log, validators } from 'utils';
 import { SubmitButton } from 'components/Buttons';
-
-import { routes } from 'screens/consts';
-import { styles } from './styles';
+import { Logo, ScreenTitle, Text, TextLink, Title, useSnackbar, View } from 'components/Common';
+import { TextInput } from 'components/Forms';
+import { isCognitoErrResponse, useAuth } from 'core/api';
+import React, { ChangeEvent, FC, useState } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
+import { routes } from 'screens/consts';
+import { globalStyles, m, StyleProps, Styles } from 'styles';
+import { errToStr, Log, polishers, validators } from 'utils';
 
 const log = Log('screens.AuthRecoverPass');
 
@@ -24,12 +22,23 @@ interface FormData {
   email?: string;
 }
 
+type FormErrs = Partial<Record<keyof FormData, string>> & { request?: string };
+
+const getFormErrs = (data: FormData): FormErrs | undefined => {
+  const emailErr = validators.getEmailErr(data.email);
+  return emailErr ? { email: emailErr } : undefined;
+};
+
+const polishFormData = (data: FormData): FormData => ({
+  email: polishers.clearEmail(data.email),
+});
+
 export const AuthRecoverPassScreen: FC<Props> = () => {
   const history = useHistory();
   const location = useLocation<LocationState | undefined>();
 
   const [data, setData] = useState<FormData>({ email: location.state?.email });
-  const [reqErr, setReqErr] = useState<string | undefined>();
+  const [errs, setErrs] = useState<FormErrs | undefined>(undefined);
   const [processing, setProcessing] = useState<boolean>(false);
 
   const { forgotPassowrd } = useAuth();
@@ -41,16 +50,21 @@ export const AuthRecoverPassScreen: FC<Props> = () => {
     event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
   ) => {
     const { value } = event.currentTarget;
-    setData({ ...data, [key]: value });
+    setErrs(undefined);
+    setData(polishFormData({ ...data, [key]: value }));
   };
 
   const handleSubmitPress = async () => {
+    log.debug('handle submit press');
     if (!email) {
       return;
     }
-    log.debug('handle submit press');
+    const curErrs = getFormErrs(data);
+    if (curErrs) {
+      return setErrs(curErrs);
+    }
     try {
-      setReqErr(undefined);
+      setErrs(undefined);
       setProcessing(true);
 
       log.debug('send recover pass request with email=', email);
@@ -62,7 +76,7 @@ export const AuthRecoverPassScreen: FC<Props> = () => {
     } catch (err) {
       log.err('recover pass err=', err);
       setProcessing(false);
-      setReqErr(isCognitoErrResponse(err) ? err.message : errToStr(err));
+      setErrs({ request: isCognitoErrResponse(err) ? err.message : errToStr(err) });
     }
   };
 
@@ -71,12 +85,12 @@ export const AuthRecoverPassScreen: FC<Props> = () => {
       <ScreenTitle title="Password Recovery" />
       <AuthScreenBackground>
         <Logo style={styles.logo} />
-        <AuthFormContainer style={{ maxWidth: 592 }}>
-          <Grid container justify="center" style={{ marginBottom: 20 }}>
-            <Title type="h3" style={m(globalStyles.authTitle, styles.authTitle)}>
+        <AuthFormContainer style={styles.container}>
+          <Grid container justify="center" style={styles.form}>
+            <Title type="h3" style={m(globalStyles.authTitle, styles.title)}>
               recover your password
             </Title>
-            <Text style={m(globalStyles.authSubtitle, styles.authSubtitle)}>
+            <Text style={m(globalStyles.authSubtitle, styles.subtitle)}>
               Enter your email and we will send you instructions to generate a new password.
             </Text>
             <Grid item xs={12}>
@@ -84,17 +98,23 @@ export const AuthRecoverPassScreen: FC<Props> = () => {
                 value={email || ''}
                 type="email"
                 valid={!validators.getEmailErr(email)}
+                error={!!errs?.email}
+                helperText={errs?.email}
                 label="Email address"
                 onChange={handleTextFieldChanged('email')}
               />
             </Grid>
           </Grid>
           <Grid container justify="center" spacing={2}>
-            <View style={reqErr ? globalStyles.authErrWrap : undefined} justifyContent="center" alignItems="center">
-              {!!reqErr && <Text style={globalStyles.authErr}>{reqErr}</Text>}
+            <View
+              style={errs?.request ? globalStyles.authErrWrap : undefined}
+              justifyContent="center"
+              alignItems="center"
+            >
+              {!!errs?.request && <Text style={globalStyles.authErr}>{errs.request}</Text>}
             </View>
           </Grid>
-          <Grid container justify="center" spacing={2} style={{ marginBottom: 40 }}>
+          <Grid container justify="center" spacing={2} style={styles.submitBtn}>
             <Grid item xs={12} sm={6} style={globalStyles.inputItem}>
               <SubmitButton processing={processing} disabled={processing || !email} onClick={handleSubmitPress}>
                 submit
@@ -108,6 +128,27 @@ export const AuthRecoverPassScreen: FC<Props> = () => {
       </AuthScreenBackground>
     </>
   );
+};
+
+const styles: Styles = {
+  container: { maxWidth: 592 },
+  logo: {
+    marginBottom: 50,
+  },
+  title: {
+    marginBottom: 20,
+  },
+  subtitle: {
+    marginBottom: 30,
+  },
+  form: {
+    marginBottom: 28,
+  },
+  linkBack: {
+    textTransform: 'capitalize',
+    textDecoration: 'underline',
+  },
+  submitBtn: { marginBottom: 40, marginTop: 0 },
 };
 
 export default AuthRecoverPassScreen;
