b5c5b11b99dfb06dedffd2374c390654119b203a
Jaroslav Khorishchenko <websnipter@gmail.com>
2021-03-14 10:13:43 +0200

    Storybook: additional viewports added

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -21,29 +21,69 @@ export const parameters = {
         },
         type: 'mobile',
       },
-      mobileLG: {
-        name: 'Large mobile (414 x 896)',
+      mobileMD: {
+        name: 'Small mobile (375 x 812)',
         styles: {
-          width: '414px',
+          width: '375px',
+          height: '812px',
+        },
+        type: 'mobile',
+      },
+      mobileLG: {
+        name: 'Large mobile (599 x 896)',
+        styles: {
+          width: '599px',
           height: '896px',
         },
         type: 'mobile',
       },
-      tablet: {
-        name: 'Tablet (1112 x 834)',
+      tabletSM: {
+        name: 'Tablet (600 x 834)',
         styles: {
-          height: '1112px',
+          height: '600px',
           width: '834px',
         },
         type: 'tablet',
       },
-      desktop: {
+      tabletMD: {
+        name: 'Tablet (1194 x 834)',
+        styles: {
+          height: '1194px',
+          width: '834px',
+        },
+        type: 'tablet',
+      },
+      tabletLG: {
+        name: 'Tablet (1279 x 720)',
+        styles: {
+          height: '1279px',
+          width: '720px',
+        },
+        type: 'tablet',
+      },
+      desktopSM: {
+        name: 'Desktop (1280 x 720)',
+        styles: {
+          height: '1280px',
+          width: '720px',
+        },
+        type: 'desktop',
+      },
+      desktopMD: {
+        name: 'Desktop (1366 x 768)',
+        styles: {
+          height: '1366px',
+          width: '768px',
+        },
+        type: 'desktop',
+      },
+      desktopLG: {
         name: 'Desktop (1920 x 1080)',
         styles: {
           height: '1920px',
           width: '1080px',
         },
-        type: 'tablet',
+        type: 'desktop',
       },
     },
   },
