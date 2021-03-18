a5f19750b6717b82dbd0bd9fdfc5a7c5b645a718
Jaroslav Khorishchenko <websnipter@gmail.com>
2021-03-18 16:36:23 +0200

    Components: LineAwesome component fixed for social icons

--- src/components/Icons/LineAwesome/index.tsx
+++ src/components/Icons/LineAwesome/index.tsx
@@ -11,9 +11,16 @@ interface Props extends StyleProps {
   className?: string;
 }
 
+const getRootClassName = (val: LineAwesomeIconType): string => {
+  if (['facebook', 'google', 'instagram', 'twitter'].includes(val)) {
+    return 'lab';
+  }
+  return 'las';
+};
+
 export const LineAwesomeIcon: FC<Props> = ({ style, type, color, size, className }) => {
   const containerStyle = ms(!!color && { color }, !!size && { fontSize: `${size}px` }, style);
-  return <Icon style={containerStyle} className={mc(`las la-${type}`, className)} />;
+  return <Icon style={containerStyle} className={mc(`${getRootClassName(type)} la-${type}`, className)} />;
 };
 
 export { LineAwesomeIconType } from './types';
--- src/components/Icons/LineAwesome/story.tsx
+++ src/components/Icons/LineAwesome/story.tsx
@@ -1,31 +1,31 @@
-import { View } from 'components/Common';
 import React from 'react';
-import { StoryMeta, Story } from 'styles';
+import { colors, sbAutoDetectActionProps, Story, StoryMeta } from 'styles';
 
 import LineAwesomeIcon, { LineAwesomeIconProps as Props } from '.';
 
 export default ((): StoryMeta<Props> => ({
   title: 'components/Icons/LineAwesome',
   component: LineAwesomeIcon,
+  args: {
+    type: 'skull-crossbones',
+  },
   argTypes: {
     color: {
       control: 'color',
-      defaultValue: '#000',
+      defaultValue: colors.back,
     },
     size: {
       control: 'range',
-      defaultValue: 20,
+      defaultValue: 30,
       min: 0,
       max: 100,
       step: 1,
     },
   },
+  parameters: {
+    layout: 'centered',
+    ...sbAutoDetectActionProps,
+  },
 }))();
 
-export const Basic: Story<Props> = args => (
-  <View row style={{ display: 'flex', width: 300, padding: 20 }}>
-    <LineAwesomeIcon {...args} type="skull-crossbones" />
-    <LineAwesomeIcon {...args} type="radiation-alt" />
-    <LineAwesomeIcon {...args} type="envira" />
-  </View>
-);
+export const Basic: Story<Props> = props => <LineAwesomeIcon {...props} />;
