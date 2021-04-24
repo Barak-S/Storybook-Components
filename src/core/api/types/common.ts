504c338e3176ec4de7b379304dba18d6e8ff038b
Jaroslav Khorishchenko <websnipter@gmail.com>
2021-04-24 08:54:56 +0300

    Social type updated

--- src/core/api/types/common.ts
+++ src/core/api/types/common.ts
@@ -1,11 +1,16 @@
 // Socials
 
 export interface Social {
-  type: SocialType;
-  name?: string;
+  id: string;
+  name: string;
   url: string;
 }
 
 export type SocialType = 'facebook' | 'twitter' | 'linkedin' | 'instagram' | 'custom';
 
 export const socialTypeArr: SocialType[] = ['facebook', 'twitter', 'linkedin', 'instagram', 'custom'];
+
+export interface Props {
+  items: Social[];
+  onChange?: (newItems: Social[]) => void;
+}
