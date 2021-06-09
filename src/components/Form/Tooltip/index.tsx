1b07bab7ae824cc0648419ceddb87a15d8fe5a93
Jaroslav Khorishchenko <websnipter@gmail.com>
2021-06-09 16:23:23 +0300

    DO-102: In mobile version hint is not shown after clicking on hint icon (only icon's color is changing)

--- src/components/Form/Tooltip/index.tsx
+++ src/components/Form/Tooltip/index.tsx
@@ -1,6 +1,6 @@
-import { Grid, Tooltip, TooltipProps } from '@material-ui/core';
+import { ClickAwayListener, Grid, Tooltip, TooltipProps } from '@material-ui/core';
 import { LineAwesomeIcon } from 'components/Icons';
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { colors, mx, StyleProps, Styles, useHover } from 'styles';
 
 interface Props extends StyleProps {
@@ -11,13 +11,30 @@ interface Props extends StyleProps {
 export const FormTooltip: FC<Props> = ({ style, placement = 'top-start', title }) => {
   const { hover, hoverProps } = useHover();
   const styles = getStyles(hover);
+  const [visible, setVisible] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!hover && visible) {
+      setVisible(false);
+    }
+  }, [hover]);
+
+  const handleClick = () => {
+    setVisible(true);
+  };
+
+  const handleClickAway = () => {
+    if (visible) setVisible(false);
+  };
 
   return (
-    <Tooltip style={style} arrow placement={placement} title={title}>
-      <Grid style={styles.iconHolder} {...hoverProps}>
-        <LineAwesomeIcon type="question-circle" style={{ color: 'inherit' }} />
-      </Grid>
-    </Tooltip>
+    <ClickAwayListener onClickAway={handleClickAway}>
+      <Tooltip style={style} arrow placement={placement} title={title} open={visible || hover}>
+        <Grid style={styles.iconHolder} {...hoverProps} onClick={handleClick}>
+          <LineAwesomeIcon type="question-circle" style={{ color: 'inherit' }} />
+        </Grid>
+      </Tooltip>
+    </ClickAwayListener>
   );
 };
 
